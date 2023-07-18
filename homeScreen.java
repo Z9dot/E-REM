d5b3736da29eb7d32f58970379585099029e4627
Z9dot <zainhashmi692@gmail.com>
2023-07-18 09:38:49 +0500

    Modified the login page interface and home screen interface

--- homeScreen.java
+++ homeScreen.java
@@ -1,44 +1,65 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.net.*;
-
 
 class homeScreen extends JFrame implements ActionListener {
     public static void main(String[] args) throws IOException {
         homeScreen hs = new homeScreen();
         hs.setVisible(true);
 
-
-        }
-
+    }
 
     JPanel home1;
     JPanel home2;
     JPanel homeSidebar;
     JPanel homeSidebarUpper;
 
-    private JButton b2;
+    // Adding all buttons here so that we can set their actions using 1 method
+    // instead of separate methods for all
+    JButton dashboard_button;
+    JButton composeMail_Button;
+    JButton bulkMail_Button;
+    JButton manageContact_Button;
+    JButton logOut;
 
-
-
-    // Adding all buttons here so that we can set their actions using 1 method instead of separate methods for all
-    JButton sidebarButton1;
-    JButton sidebarButton2;
-    JButton sidebarButton3;
-    JButton sidebarButton4;
-    JButton sidebarButton5;
-    JButton sidebarButton6;
     private String b2val;
 
+    private JButton addNewButton(JButton button, String title, String pathname) throws IOException {
 
+        BufferedImage image = ImageIO.read(new File(pathname));
+        Image icon = image.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        button = new JButton();
+        button.setText(title);
 
+        button.setFont(new Font("Open Sans", Font.BOLD, 18));
+        button.setFocusable(false);
+        button.setIcon(new ImageIcon(icon));
+        button.setHorizontalAlignment(AbstractButton.LEFT);
+        button.setVerticalTextPosition(AbstractButton.CENTER);
+        button.setIconTextGap(20);
+        button.setBackground(Color.WHITE);
+        button.setMaximumSize(new Dimension(350, 50));
+        return button;
+    }
+
+    private Component accountCredentials(String emailAddress, String imagePath) throws IOException {
+        // The account icon section
+        BufferedImage image = ImageIO.read(new File(imagePath));
+        Image icon = image.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        JLabel accCredentials = new JLabel();
+        accCredentials.setText("<html>" + emailAddress + "<br>Status: Active</html>");
+        accCredentials.setFont(new Font(Font.SERIF, Font.BOLD, 15));
+        accCredentials.setIcon(new ImageIcon(icon));
+        return accCredentials;
+    }
 
     public homeScreen() throws IOException {
 
@@ -47,9 +68,8 @@ class homeScreen extends JFrame implements ActionListener {
         setResizable(false);
         setTitle("E-REM V 1.0");
         setLayout(null);
-        //hS.setLocationRelativeTo(null);
+        // hS.setLocationRelativeTo(null);
         getContentPane().setBackground(new Color(244, 246, 248));
-
 
         home1 = new JPanel();
         home1.setBackground(new Color(244, 246, 248));
@@ -57,13 +77,12 @@ class homeScreen extends JFrame implements ActionListener {
         home1.setLayout(h1L);
         home1.setBounds(300, 0, 1100, 140);
 
-
         // ICON 1 OF BTN1
         ImageIcon btn1Image = new ImageIcon("email.png");
         Image btn1modified = btn1Image.getImage().getScaledInstance(48, 48, Image.SCALE_SMOOTH);
         btn1Image = new ImageIcon(btn1modified);
 
-        //ICON 2 OF BTN 2
+        // ICON 2 OF BTN 2
         ImageIcon btn2Image = new ImageIcon("tasks.png");
         Image btn2modified = btn2Image.getImage().getScaledInstance(48, 48, Image.SCALE_SMOOTH);
         btn2Image = new ImageIcon(btn2modified);
@@ -71,59 +90,35 @@ class homeScreen extends JFrame implements ActionListener {
         ImageIcon btn3Image = new ImageIcon("contacts.png");
         Image btn3modified = btn3Image.getImage().getScaledInstance(48, 48, Image.SCALE_SMOOTH);
         btn3Image = new ImageIcon(btn3modified);
-        //ICON 3 OF BTN 3
+        // ICON 3 OF BTN 3
 
+        // accCredentials.setHorizontalAlignment(JLabel.CENTER);
 
-        //  accCredentials.setHorizontalAlignment(JLabel.CENTER);
+        JButton sent_Mails = new JButton();
+        sent_Mails.setText("5");
+        sent_Mails.setFont(new Font("Calibri", Font.BOLD, 35));
+        sent_Mails.setFocusable(false);
+        sent_Mails.setMaximumSize(new Dimension(280, 80));
+        sent_Mails.setBackground(new Color(244, 246, 248));
+        sent_Mails.setIcon(btn1Image);
+        sent_Mails.setVerticalTextPosition(AbstractButton.CENTER);
+        sent_Mails.setHorizontalTextPosition(AbstractButton.LEADING);
+        sent_Mails.setIconTextGap(180);
+        sent_Mails.setBorder(new TitledBorder("SENT EMAILS"));
 
-        JButton b1 = new JButton();
-        b1.setText("5");
-        b1.setFont(new Font("Calibri", Font.BOLD, 35));
-        b1.setFocusable(false);
-        b1.setMaximumSize(new Dimension(280, 80));
-        b1.setBackground(new Color(244, 246, 248));
-        b1.setIcon(btn1Image);
-        b1.setVerticalTextPosition(AbstractButton.CENTER);
-        b1.setHorizontalTextPosition(AbstractButton.LEADING);
-        b1.setIconTextGap(180);
-        b1.setBorder(new TitledBorder("SENT EMAILS"));
-
-        b2 = new JButton();
-        b2.setText("12");
-        b2val= String.valueOf(b2.getText());
-        b2.setFont(new Font("Calibri", Font.BOLD, 35));
-        b2.setFocusable(false);
-        b2.setMaximumSize(new Dimension(280, 80));
-        b2.setBackground(new Color(244, 246, 248));
-        b2.setIcon(btn2Image);
-        // b1.setHorizontalAlignment(JButton.LEFT);
-        b2.setVerticalTextPosition(AbstractButton.CENTER);
-        b2.setHorizontalTextPosition(AbstractButton.LEADING);
-        b2.setIconTextGap(180);
-        b2.setBorder(new TitledBorder("OVERDUE TASKS"));
-
-      //  btnValue= b2.getText();
-
-
-        JButton b3 = new JButton();
-        b3.setText("15");
-        b3.setFont(new Font("Calibri", Font.BOLD, 35));
-        b3.setFocusable(false);
-        b3.setMaximumSize(new Dimension(280, 80));
-        b3.setBackground(new Color(244, 246, 248));
-        b3.setIcon(btn3Image);
-        // b1.setHorizontalAlignment(JButton.LEFT);
-        b3.setVerticalTextPosition(AbstractButton.CENTER);
-        b3.setHorizontalTextPosition(AbstractButton.LEADING);
-        b3.setIconTextGap(180);
-        b3.setBorder(new TitledBorder("TOTAL CONTACTS"));
-
-
-        home2 = new JPanel();
-        BoxLayout h2L = new BoxLayout(home2, BoxLayout.Y_AXIS);
-        home2.setLayout(h2L);
-        home2.setBackground(new Color(244, 246, 248));
-        home2.setBounds(300, 140, 1050, 600);
+        // btnValue= b2.getText();
+        JButton contacts_Count = new JButton();
+        contacts_Count.setText("15");
+        contacts_Count.setFont(new Font("Calibri", Font.BOLD, 35));
+        contacts_Count.setFocusable(false);
+        contacts_Count.setMaximumSize(new Dimension(280, 80));
+        contacts_Count.setBackground(new Color(244, 246, 248));
+        contacts_Count.setIcon(btn3Image);
+        // sent_Mails.setHorizontalAlignment(JButton.LEFT);
+        contacts_Count.setVerticalTextPosition(AbstractButton.CENTER);
+        contacts_Count.setHorizontalTextPosition(AbstractButton.LEADING);
+        contacts_Count.setIconTextGap(180);
+        contacts_Count.setBorder(new TitledBorder("TOTAL CONTACTS"));
 
         homeSidebar = new JPanel();
         homeSidebar.setBackground(new Color(255, 182, 117));
@@ -131,7 +126,6 @@ class homeScreen extends JFrame implements ActionListener {
         BoxLayout h3L = new BoxLayout(homeSidebar, BoxLayout.Y_AXIS);
         homeSidebar.setLayout(h3L);
         homeSidebar.setBounds(0, 120, 300, 780);
-
 
         homeSidebarUpper = new JPanel();
         BoxLayout hsuL = new BoxLayout(homeSidebarUpper, BoxLayout.Y_AXIS);
@@ -141,165 +135,57 @@ class homeScreen extends JFrame implements ActionListener {
         homeSidebarUpper.setBounds(0, 0, 300, 250);
         homeSidebarUpper.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        // The account icon section
-        ImageIcon accIcon = new ImageIcon("account icon tp.png");
-        Image aiOriginal = accIcon.getImage();
-        Image aiModified = aiOriginal.getScaledInstance(60, 60, Image.SCALE_SMOOTH);
-        accIcon = new ImageIcon(aiModified);
-        JLabel accCredentials = new JLabel();
-        accCredentials.setText("<html>digitalhamayun@gmail.com<br>Status: Active</html>");
-        accCredentials.setFont(new Font(Font.SERIF, Font.BOLD, 15));
-        accCredentials.setIcon(accIcon);
+        dashboard_button = addNewButton(new JButton(), "Dashboard", "dashboard.png");
+        composeMail_Button = addNewButton(new JButton(), "Compose Mail", "single email.png");
+        bulkMail_Button = addNewButton(new JButton(), "Bulk Email", "bulk email.png");
+        manageContact_Button = addNewButton(new JButton(), "Contacts", "contact manager.png");
 
-        ImageIcon sbi1 = new ImageIcon("dashboard.png");
-        Image sbi1Modified = sbi1.getImage().getScaledInstance(38, 38, Image.SCALE_SMOOTH);
-        sidebarButton1 = new JButton();
-        sidebarButton1.setText("DASHBOARD");
-        sidebarButton1.setBackground(new Color(244, 246, 248));
-        sidebarButton1.setFont(new Font("Open Sans", Font.BOLD, 16));
-        sidebarButton1.setFocusable(false);
-        sidebarButton1.setIcon(new ImageIcon(sbi1Modified));
-
-        sidebarButton1.setHorizontalAlignment(AbstractButton.LEFT);
-        sidebarButton1.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton1.setIconTextGap(20);
-        sidebarButton1.setForeground(Color.BLACK);
-
-        sidebarButton1.setMaximumSize(new Dimension(350, 50));
-
-        ImageIcon sbi2 = new ImageIcon("single email.png");
-        Image sbi2Modified = sbi2.getImage().getScaledInstance(42, 42, Image.SCALE_SMOOTH);
-        sidebarButton2 = new JButton();
-        sidebarButton2.setText("COMPOSE EMAIL");
-        sidebarButton2.setFont(new Font("Open Sans", Font.BOLD, 16));
-        sidebarButton2.setFocusable(false);
-        sidebarButton2.setIcon(new ImageIcon(sbi2Modified));
-
-        sidebarButton2.setHorizontalAlignment(AbstractButton.LEFT);
-        sidebarButton2.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton2.setIconTextGap(20);
-        sidebarButton2.setBackground(Color.WHITE);
-        sidebarButton2.setMaximumSize(new Dimension(300, 50));
-
-        ImageIcon sbi3 = new ImageIcon("bulk email.png");
-        Image sbi3Modified = sbi3.getImage().getScaledInstance(42, 42, Image.SCALE_SMOOTH);
-        sidebarButton3 = new JButton();
-        sidebarButton3.setText("BULK EMAIL");
-
-        sidebarButton3.setFont(new Font("Open Sans", Font.BOLD, 16));
-        sidebarButton3.setFocusable(false);
-        sidebarButton3.setIcon(new ImageIcon(sbi3Modified));
-        sidebarButton3.setHorizontalAlignment(AbstractButton.LEFT);
-        sidebarButton3.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton3.setIconTextGap(20);
-        sidebarButton3.setBackground(Color.WHITE);
-        sidebarButton3.setMaximumSize(new Dimension(350, 50));
-
-        ImageIcon sbi4 = new ImageIcon("task manager.png");
-        Image sbi4Modified = sbi4.getImage().getScaledInstance(42, 42, Image.SCALE_SMOOTH);
-        sidebarButton4 = new JButton();
-        sidebarButton4.setText("TASK MANAGER");
-
-        sidebarButton4.setFont(new Font("Open Sans", Font.BOLD, 16));
-        sidebarButton4.setFocusable(false);
-        sidebarButton4.setIcon(new ImageIcon(sbi4Modified));
-
-        sidebarButton4.setHorizontalAlignment(AbstractButton.LEFT);
-        sidebarButton4.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton4.setIconTextGap(20);
-        sidebarButton4.setBackground(Color.WHITE);
-        sidebarButton4.setMaximumSize(new Dimension(350, 50));
-
-        ImageIcon sbi5 = new ImageIcon("contact manager.png");
-        Image sbi5Modified = sbi5.getImage().getScaledInstance(42, 42, Image.SCALE_SMOOTH);
-        sidebarButton5 = new JButton();
-        sidebarButton5.setText("MANAGE CONTACTS");
-
-        sidebarButton5.setFont(new Font("Open Sans", Font.BOLD, 16));
-        sidebarButton5.setFocusable(false);
-        sidebarButton5.setIcon(new ImageIcon(sbi5Modified));
-
-        sidebarButton5.setHorizontalAlignment(AbstractButton.LEFT);
-        sidebarButton5.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton5.setIconTextGap(20);
-        sidebarButton5.setBackground(Color.WHITE);
-        sidebarButton5.setMaximumSize(new Dimension(350, 50));
-
-        ImageIcon sbi6 = new ImageIcon("gmail.png");
-        Image sbi6Modified = sbi6.getImage().getScaledInstance(55, 55, Image.SCALE_SMOOTH);
-        sidebarButton6 = new JButton();
-        sidebarButton6.setText(" ");
-        sidebarButton6.setFocusable(true);
-        sidebarButton6.setIcon(new ImageIcon(sbi6Modified));
-        sidebarButton6.setBorder(null);
-        sidebarButton6.setVerticalTextPosition(AbstractButton.CENTER);
-        sidebarButton6.setBackground(null);
-        // sidebarButton6.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
-
-        sidebarButton6.setHorizontalTextPosition(AbstractButton.LEADING);
-        sidebarButton6.setIconTextGap(102);
-
-
-        home1.add(Box.createRigidArea(new Dimension(85, 0)));
-        home1.add(b1);
-        home1.add(Box.createRigidArea(new Dimension(25, 0)));
-        home1.add(b2);
-        home1.add(Box.createRigidArea(new Dimension(25, 0)));
-        home1.add(b3);
+        home1.add(Box.createRigidArea(new Dimension(100, 0)));
+        home1.add(sent_Mails);
+        home1.add(Box.createRigidArea(new Dimension(50, 0)));
+        home1.add(contacts_Count);
         home1.add(Box.createRigidArea(new Dimension(15, 0)));
 
         homeSidebarUpper.add(Box.createRigidArea(new Dimension(44, 25)));
-        homeSidebarUpper.add(accCredentials);
+        homeSidebarUpper.add(accountCredentials("email_Address", "account icon tp.png"));
 
-        homeSidebar.add(Box.createRigidArea(new Dimension(0, 30)));
-        homeSidebar.add(sidebarButton1);
         homeSidebar.add(Box.createRigidArea(new Dimension(0, 50)));
-        homeSidebar.add(sidebarButton2);
-        homeSidebar.add(Box.createRigidArea(new Dimension(0, 30)));
-        homeSidebar.add(sidebarButton3);
-        homeSidebar.add(Box.createRigidArea(new Dimension(0, 30)));
-        homeSidebar.add(sidebarButton4);
-        homeSidebar.add(Box.createRigidArea(new Dimension(0, 30)));
-        homeSidebar.add(sidebarButton5);
+        homeSidebar.add(dashboard_button);
+        homeSidebar.add(Box.createRigidArea(new Dimension(0, 50)));
+        homeSidebar.add(composeMail_Button);
+        homeSidebar.add(Box.createRigidArea(new Dimension(0, 50)));
+        homeSidebar.add(bulkMail_Button);
+        homeSidebar.add(Box.createRigidArea(new Dimension(0, 50)));
+        homeSidebar.add(manageContact_Button);
         homeSidebar.add(Box.createRigidArea(new Dimension(0, 75)));
-        homeSidebar.add(sidebarButton6);
-
 
         // Adding actions to sidebar buttons
-        sidebarButton1.addActionListener(this);
-        sidebarButton2.addActionListener(this);
-        sidebarButton3.addActionListener(this);
-        sidebarButton4.addActionListener(this);
-        sidebarButton5.addActionListener(this);
-        sidebarButton6.addActionListener(this);
-
-
-
+        dashboard_button.addActionListener(this);
+        composeMail_Button.addActionListener(this);
+        bulkMail_Button.addActionListener(this);
+        manageContact_Button.addActionListener(this);
 
         home1.setVisible(true);
-        home2.setVisible(true);
+        // home2.setVisible(true);
 
         homeSidebar.setVisible(true);
         homeSidebarUpper.setVisible(true);
 
-
         add(home1);
-        add(home2);
+        // add(home2);
         add(homeSidebar);
         add(homeSidebarUpper);
 
-
-
-
     }
-    public String getB2val(){
+
+    public String getB2val() {
         return b2val;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == sidebarButton1) {
-            homeScreen hs= null;
+        if (e.getSource() == dashboard_button) {
+            homeScreen hs = null;
             try {
                 hs = new homeScreen();
             } catch (IOException ex) {
@@ -308,8 +194,8 @@ class homeScreen extends JFrame implements ActionListener {
             hs.setVisible(true);
             dispose();
         }
-        if (e.getSource() == sidebarButton2) {
-            sendSingleEmail sse= null;
+        if (e.getSource() == composeMail_Button) {
+            sendSingleEmail sse = null;
             try {
                 sse = new sendSingleEmail(null, null, null);
             } catch (IOException ex) {
@@ -319,8 +205,8 @@ class homeScreen extends JFrame implements ActionListener {
             dispose();
 
         }
-        if (e.getSource() == sidebarButton3) {
-            sendBulkEmail sbe= null;
+        if (e.getSource() == bulkMail_Button) {
+            sendBulkEmail sbe = null;
             try {
                 sbe = new sendBulkEmail();
             } catch (IOException ex) {
@@ -329,23 +215,24 @@ class homeScreen extends JFrame implements ActionListener {
             sbe.setVisible(true);
             dispose();
         }
-        if (e.getSource() == sidebarButton4) {
-            // open tasks management frame
-        }
-        if (e.getSource() == sidebarButton5) {
+
+        if (e.getSource() == manageContact_Button) {
             // open contacts management frame
         }
-        if (e.getSource() == sidebarButton6) {
-            // open gmail account of user on browser
-            try {
-                Desktop userDesktop = java.awt.Desktop.getDesktop();
 
-
-                URI Url = new URI("https://gmail.com");
-                userDesktop.browse(Url);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 }
+
+/*
+ * if (e.getSource() == sidebarButton6) {
+ * // open gmail account of user on browser
+ * try {
+ * Desktop userDesktop = java.awt.Desktop.getDesktop();
+ * 
+ * URI Url = new URI("https://gmail.com");
+ * userDesktop.browse(Url);
+ * } catch (Exception ex) {
+ * ex.printStackTrace();
+ * }
+ * }
+ */
--- loginScreen.java
+++ loginScreen.java
@@ -1,165 +1,94 @@
-import java.io.IOException;
-import java.io.StreamCorruptedException;
-import java.io.*;
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.*;
-public class loginScreen extends JFrame{
-    public static void main(String[] args){
-        loginScreen lScreen= new loginScreen();
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class loginScreen extends JFrame {
+    public static void main(String[] args) {
+        loginScreen lScreen = new loginScreen();
         lScreen.setVisible(true);
     }
 
     private String userPassword;
     private String userEmail;
+
     public loginScreen() {
-        JFrame f1 = new JFrame();
-        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f1.setSize(1366, 768);
-        f1.setResizable(false);
-        f1.setTitle("E-REM V 1.0");
-        f1.setLocationRelativeTo(null);
-        f1.getContentPane().setBackground(new Color(233, 242, 246));
+        // Setting login frame
+        setTitle("Login");
+        setSize(500, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
 
-        JPanel pleft = new JPanel();
-        BoxLayout box = new BoxLayout(pleft, BoxLayout.Y_AXIS);
-        pleft.setLayout(box);
-        pleft.setBackground(Color.white);
-        pleft.setBounds(0, 0, 600, 768);
+        JPanel IconPanel, CredentialsPanel;
 
+        IconPanel = new JPanel(new GridLayout(3, 0));
+        ImageIcon icon = new ImageIcon("logo icon.png", "E-REM");
+        JLabel image = new JLabel(icon);
+        IconPanel.add(image);
 
-        // pleft.setBorder(new LineBorder(Color.ORANGE, 3));
-        JButton btn= new JButton();
-        btn.setBackground(Color.gray);
-        btn.setMaximumSize(new Dimension(400, 600));
-        btn.setBorder(null);
-        btn.setFocusable(false);
+        add(IconPanel);
+        JLabel emailLabelField = new JLabel(" Email ");
+        emailLabelField.setFont(new Font("Ariel", Font.PLAIN, 16));
+        JTextField emailAddressField = new JTextField(10);
+        emailAddressField.setFont(new Font("Ariel", Font.PLAIN, 16));
 
-        JPanel pright = new JPanel();
-        pright.setLayout(null);
-        pright.setBounds(100, 0, 766, 768);
-        pright.setBackground(Color.ORANGE);
+        JLabel passwordLabeField = new JLabel(" Password");
+        passwordLabeField.setFont(new Font("Ariel", Font.PLAIN, 16));
+        JPasswordField passwordField = new JPasswordField(10);
+        passwordField.setFont(new Font("Ariel", Font.PLAIN, 16));
 
-        pright.add(btn);
+        CredentialsPanel = new JPanel(new GridLayout(5, 2));
+        CredentialsPanel.setPreferredSize(new Dimension(500, 400));
 
-        // THE LOGIN FIELDS PANEL
-        // JPanel loginFields= new JPanel();
-        // BoxLayout boxLogin= new BoxLayout(loginFields, BoxLayout.Y_AXIS);
-        //loginFields.setLayout(boxLogin);
+        JPanel buttonPanel = new JPanel(null);
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(150, 70, 170, 30);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(new ActionListener() {
 
-        // gl.setVgap(1);
-
-        // loginFields.setBounds(100, 100, loginFields.getWidth(), loginFields.getHeight());
-        JLabel emailL = new JLabel("Enter Email: ");
-        //   emailL.setAlignmentX(Component.LEFT_ALIGNMENT);
-        JTextField enterEmail = new JTextField(2);
-        userEmail= enterEmail.getText();
-
-        //enterEmail.setAlignmentX(Component.LEFT_ALIGNMENT);
-        enterEmail.setMaximumSize(new Dimension(300, 40));
-        //  enterEmail.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        //enterEmail.setMaximumSize(new Dimension(700,100));
-
-        JLabel passL = new JLabel("Enter Password: ");
-        JTextField enterPass = new JTextField(2);
-        userPassword= enterPass.getText();
-        enterPass.setMaximumSize(new Dimension(300, 40));
-        // enterPass.setAlignmentX(Component.CENTER_ALIGNMENT);
-        // enterPass.setMaximumSize(new Dimension(700,100));
-
-        //enterEmail.setAlignmentX(Component.CENTER_ALIGNMENT);
-        // enterPass.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-
-        //  loginFields.add(Box.createVerticalGlue());
-
-
-        //  loginFields.setBounds(-30, 200, 600, 300);
-        //   loginFields.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-
-        ImageIcon logo = new ImageIcon("erem logo.png");
-        Image logoModified = logo.getImage().getScaledInstance(105, 145, Image.SCALE_SMOOTH);
-        logo = new ImageIcon(logoModified);
-        JLabel logoL = new JLabel(logo);
-        logoL.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //logoL.setBounds(-30, -10, 600, 300);
-        // label.setHorizontalAlignment(JLabel.CENTER);
-        //label.setBackground(new Color(000000));
-        //label.setFont(new Font("Verdana", Font.PLAIN, 25));
-        //  logo.setVisible(true);
-
-
-        // The account icon section
-        ImageIcon accIcon = new ImageIcon("account icon.png");
-        Image aiOriginal = accIcon.getImage();
-        Image aiModified = aiOriginal.getScaledInstance(48, 48, Image.SCALE_SMOOTH);
-        accIcon = new ImageIcon(aiModified);
-
-        JLabel accCredentials = new JLabel();
-        accCredentials.setText("<html>digitalhamayun@gmail.com<br>Status: Active</html>");
-        accCredentials.setIcon(accIcon);
-        //  accCredentials.setHorizontalAlignment(JLabel.CENTER);
-
-
-        // The log in button
-
-
-        ImageIcon loginButtonImage = new ImageIcon("sign in button.png");
-        Image lbiModified = loginButtonImage.getImage().getScaledInstance(255, 80, Image.SCALE_SMOOTH);
-        JButton logIn = new JButton(new ImageIcon(lbiModified));
-        logIn.setMaximumSize(new Dimension(245, 60));
-        logIn.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        logIn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    homeScreen homeS = new homeScreen();
-                    homeS.setVisible(true);
-                    dispose();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
+                if (verifyLogin(getUserEmail(), getUserPassword())) {
+                    try {
+                        homeScreen homeScreen = new homeScreen();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
                 }
-
-
             }
+
         });
-        // logIn.setSize(240, 70);
 
-
-        //logIn.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        //loginButtonPanel.setMaximumSize(new Dimension(245, 60));
-        // logIn.setAlignmentY(13);
-
-
-        pleft.add(Box.createRigidArea(new Dimension(0, 70)));
-        pleft.add(logoL);
-        pleft.add(Box.createRigidArea(new Dimension(0, 100)));
-        pleft.add(emailL, Component.LEFT_ALIGNMENT);
-        pleft.add(enterEmail);
-        pleft.add(Box.createRigidArea(new Dimension(0, 30)));
-        pleft.add(passL);
-        pleft.add(enterPass);
-        pleft.add(Box.createRigidArea(new Dimension(0, 120)));
-        pleft.add(logIn);
-        pleft.setVisible(true);
-        pright.setVisible(true);
-        add(pleft);
-        add(pright);
+        CredentialsPanel.add(emailLabelField);
+        CredentialsPanel.add(emailAddressField);
+        CredentialsPanel.add(new JLabel());
+        CredentialsPanel.add(passwordLabeField);
+        CredentialsPanel.add(passwordField);
+        IconPanel.add(CredentialsPanel);
+        buttonPanel.add(loginButton);
+        IconPanel.add(buttonPanel);
 
     }
-    public String getUserEmail(){
+
+    public String getUserEmail() {
         return userEmail;
     }
-    public String getUserPassword(){
+
+    public String getUserPassword() {
         return userPassword;
     }
 
+    // Sample login verification method
+    private boolean verifyLogin(String email, String password) {
+        boolean check = false;
+        return email.equals("test@example.com") && password.equals("password");
+    }
 }
+
+// 674980207735-po7cdsn1s28gr547q58mbmdgt8621c7n.apps.googleusercontent.com
